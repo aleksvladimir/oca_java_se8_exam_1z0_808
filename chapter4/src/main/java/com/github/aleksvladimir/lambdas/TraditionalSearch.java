140672237dd9a6d9a44613e817f94c26797dec39
Vladimir Aleksandrov <aleksvladimir@list.ru>
2020-05-14 22:05:09 +0300

    Writing Simple Lambdas: was added describe how compiler figuring out what lambda expressions mean

--- chapter4/src/main/java/com/github/aleksvladimir/lambdas/TraditionalSearch.java
+++ chapter4/src/main/java/com/github/aleksvladimir/lambdas/TraditionalSearch.java
@@ -15,6 +15,19 @@ public class TraditionalSearch {
         print(animals, a -> a.canHop());        // pass lambda that does check
         print(animals, a -> a.canSwim());
         print(animals, a -> !a.canSwim());
+
+        /*
+        Java relies on context when figuring out what lambda expressions mean.
+        We are passing this lambda as the second parameter of the print() method.
+        That method expects a CheckTrait as the second parameter.
+        Since we are passing a lambda instead, Java tries to map our lambda to that interface:
+            boolean test(Animal a);
+        Java полагается на контекст когда выясняет что означает выражение лямбда.
+        Мы пропускаем эту лямбду как второй параметр метода print().
+        Этот метод ожидает CheckTrait как второй параметр.
+        Поскольку мы пропускаем лямбду вместо него, Java пытается сопоставить нашу лямбду с этим интерфейсом:
+            boolean test(Animal a);
+        * */
     }
 
     private static void print(List<Animal> animals, CheckTrait checker) {
