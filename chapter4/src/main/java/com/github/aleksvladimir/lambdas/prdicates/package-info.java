c5a83e96b4ae3bf16612d37eae5bf349eba1ad03
Vladimir Aleksandrov <aleksvladimir@list.ru>
2020-05-16 12:33:32 +0300

    Writing Simple Lambdas: Predicate. Description of the basics.

---
+++ chapter4/src/main/java/com/github/aleksvladimir/lambdas/prdicates/package-info.java
@@ -0,0 +1,20 @@
+/**
+ For the OCA exam, you only need to know how to implement lambda expressions that
+ use the Predicate interface.
+
+ Remember the one method in the interface called test()?
+ It takes any one reference type parameter and returns a boolean.
+ Functional programming is a large topic and just the basics are covered.
+ On the OCP exam, you’ll learn how to get rid of the loop entirely for more
+ than just removeIf(). You’ll also learn the rules for implementing your own
+ functional interfaces as we did with CheckTrait.
+
+ Помните один метод в интерфейсе под названием test ()?
+ Он принимает любой параметр ссылочного типа и возвращает логическое значение.
+ Функциональное программирование-это большая тема, и здесь рассматриваются только основы.
+ На экзамене OCP вы узнаете, как полностью избавиться от цикла для большего,
+ чем просто removeIf(). Вы также узнаете правила реализации собственных функциональных
+ интерфейсов, как это было сделано с CheckTrait.
+ */
+
+package com.github.aleksvladimir.lambdas.prdicates;
