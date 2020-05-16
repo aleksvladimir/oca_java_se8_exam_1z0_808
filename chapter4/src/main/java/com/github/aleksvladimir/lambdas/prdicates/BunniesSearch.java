99ba97e13914d452b44091aae9261c8d3386a2aa
Vladimir Aleksandrov <aleksvladimir@list.ru>
2020-05-16 12:27:12 +0300

    Writing Simple Lambdas: Predicate. Using Collections predicate for use lambda.

---
+++ chapter4/src/main/java/com/github/aleksvladimir/lambdas/prdicates/BunniesSearch.java
@@ -0,0 +1,26 @@
+package com.github.aleksvladimir.lambdas.prdicates;
+
+/*
+В Java 8 даже интегрированы интерфейсы предикатав некоторые существующие классы. Вам нужно
+знать только одно для экзамена. ArrayList объявляет метод removeIf(), который принимает
+предикат.
+
+Java 8 even integrated the Predicate interface into some existing classes. There is only
+one you need to know for the exam. ArrayList declares a removeIf() method that takes a
+Predicate.
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class BunniesSearch {
+    public static void main(String[] arg) {
+        List<String> bunnies = new ArrayList<>();
+        bunnies.add("long ear");
+        bunnies.add("floppy");
+        bunnies.add("hoppy");
+        System.out.println(bunnies); // [long ear, floppy, hoppy]
+        bunnies.removeIf(s -> s.charAt(0) != 'h');
+        System.out.println(bunnies); // [hoppy]
+    }
+}
