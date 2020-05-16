83b72dc61466ebccc8afbf2cc43fa352053477eb
Vladimir Aleksandrov <aleksvladimir@list.ru>
2020-05-16 18:00:03 +0300

    Writing Simple Lambdas: Answer to the review question 26

---
+++ chapter4/src/main/java/com/github/aleksvladimir/reviewquestions/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Appendix A. Answers to Review Questions
+ */
+
+/*
+26. What is the result of the following class?
+A. This code is correct. Line 8 creates a lambda expression that checks if the age is less
+than 5. Since there is only one parameter and it does not specify a type, the parentheses
+around the type parameter are optional. Line 10 uses the Predicate interface, which
+declares a test() method.
+ */
+
+package com.github.aleksvladimir.reviewquestions;
