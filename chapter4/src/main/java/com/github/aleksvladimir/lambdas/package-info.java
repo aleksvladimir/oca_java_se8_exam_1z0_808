11d8a91c393463d312c18a84a3c6a9ae3e05f5e1
Vladimir Aleksandrov <aleksvladimir@list.ru>
2020-05-16 13:24:55 +0300

    Writing Simple Lambdas: Summary. Lambdas & Predicate.

--- chapter4/src/main/java/com/github/aleksvladimir/lambdas/package-info.java
+++ chapter4/src/main/java/com/github/aleksvladimir/lambdas/package-info.java
@@ -8,4 +8,18 @@
  * @since 0.1
  */
 
+/**
+ * Summary.
+ * Lambda expressions, or lambdas, allow passing around blocks of code.
+ * The full syntax looks like (String a, String b) -> { return a.equals(b); }.
+ * The parameter types can be omitted.
+ * When only one parameter is specified without a type, the parentheses can also be omitted.
+ * The braces and return statement can be omitted for a single statement, making
+ * the short form (a -> a.equals(b)).
+ * Lambdas are passed to a method expecting an interface with one method.
+ * Predicate is a common interface. It has one method named test
+ * that returns a boolean and takes any type.
+ * The removeIf() method on ArrayList takes a Predicate.
+ */
+
 package com.github.aleksvladimir.lambdas;
